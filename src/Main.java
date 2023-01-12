e47ebb0d46017ab7cd6fa331771ef028fcda6675
junnunkarim <24603630+junnunkarim@users.noreply.github.com>
2023-01-13 02:43:09 +0600

    add studentList and courseList, ArrayList in the Main class

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Main
 {
-  static ArrayList<Student> studentList;
-  static ArrayList<Course> courseList;
+  static ArrayList<Student> studentList = new ArrayList<>();
+  static ArrayList<Course> courseList = new ArrayList<>();
   public static void main(String[] args)
   {
   }
